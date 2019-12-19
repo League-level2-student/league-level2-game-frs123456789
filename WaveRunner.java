598edd6c8ff5425440139f86f4c536ef5d16c650
League Student <league@iMac-22.attlocal.net>
2019-12-18 19:56:36 -0800

    startofwaverunnerthegame!!

---
+++ WaveRunner.java
@@ -0,0 +1,11 @@
+
+public class WaveRunner {
+	GamePanel gp = new GamePanel();
+	JFrame jf;
+	public static final int WIDTH = 500;
+	public static final int HEIGHT = 800;
+	
+	public static void main(String[] args) {
+	
+}
+}
