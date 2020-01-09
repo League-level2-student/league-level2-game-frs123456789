ed03866a1af7f79f0accd88aaf717084c0cf82f2
League Student <league@iMac-14.attlocal.net>
2020-01-08 19:54:08 -0800

    verylittlechange

--- WaveRunner.java
+++ WaveRunner.java
@@ -1,11 +1,26 @@
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
 
 public class WaveRunner {
 	GamePanel gp = new GamePanel();
 	JFrame jf;
 	public static final int WIDTH = 500;
 	public static final int HEIGHT = 800;
-	
-	public static void main(String[] args) {
-	
-}
+
+	public LeagueInvaders() {
+		jf = new JFrame();
+		jf.setVisible(true);
+		jf.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+		jf.pack();
+		jf.addKeyListener(gp);
+
+	}
+
+	void setup() {
+		jf.add(gp);
+		int height = HEIGHT;
+		int width = WIDTH;
+
+	}
 }
