0bbf54aa9286e9bf9292ef7d16a2c1c05be60bd7
League Student <league@iMac-14.attlocal.net>
2020-01-15 19:57:11 -0800

    I actually did a little bit of wooork

---
+++ GamePanel.java
@@ -0,0 +1,54 @@
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class GamePanel {
+jp = new JPanel;
+final int MENU = 0;
+final int GAME = 1;
+final int END = 2;
+
+
+
+GamePanel(){
+	
+}
+
+public void paintComponent(Graphics g) {
+	if (currentState == MENU) {
+		drawMenuState(g);
+	} else if (currentState == GAME) {
+		drawGameState(g);
+	} else if (currentState == END) {
+		drawEndState(g);
+	}
+}
+
+void updateMenuState() {
+
+}
+
+void updateGameState() {
+
+}
+
+void updateEndState() {
+
+}
+
+void drawMenuState(Graphics g) {
+	g.
+}
+
+void drawGameState(Graphics g) {
+	
+	
+	}
+
+void drawEndState(Graphics g) {
+
+
+}
+
+
+
+}
--- WaveRunner.java
+++ WaveRunner.java
@@ -3,24 +3,43 @@ import java.awt.Dimension;
 import javax.swing.JFrame;
 
 public class WaveRunner {
-	GamePanel gp = new GamePanel();
-	JFrame jf;
+
+
 	public static final int WIDTH = 500;
 	public static final int HEIGHT = 800;
-
-	public LeagueInvaders() {
+	
+	WaveRunner(){
+		WaveRunner wr = new WaveRunner();
 		jf = new JFrame();
 		jf.setVisible(true);
 		jf.setPreferredSize(new Dimension(WIDTH, HEIGHT));
 		jf.pack();
-		jf.addKeyListener(gp);
-
+		
 	}
-
 	void setup() {
-		jf.add(gp);
+		
 		int height = HEIGHT;
 		int width = WIDTH;
 
 	}
-}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	}
+
+
